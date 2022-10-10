aef7e7bd68d0d99cc7e1fa913b16e1128665a538
Template Janitor <template.janitor@example.com>
2022-10-10 18:45:27 +0000

    Cleanup template and initialize repository

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@
 #include <spdlog/spdlog.h>
 
 // This file will be generated automatically when you run the CMake
-// configuration step. It creates a namespace called `myproject`. You can modify
+// configuration step. It creates a namespace called `FtxUiTemplate`. You can modify
 // the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
 
@@ -308,7 +308,7 @@ void game_iteration_canvas()
 int main(int argc, const char **argv)
 {
   try {
-    CLI::App app{ fmt::format("{} version {}", myproject::cmake::project_name, myproject::cmake::project_version) };
+    CLI::App app{ fmt::format("{} version {}", FtxUiTemplate::cmake::project_name, FtxUiTemplate::cmake::project_version) };
 
     std::optional<std::string> message;
     app.add_option("-m,--message", message, "A message to print back out");
@@ -328,7 +328,7 @@ int main(int argc, const char **argv)
     CLI11_PARSE(app, argc, argv);
 
     if (show_version) {
-      fmt::print("{}\n", myproject::cmake::project_version);
+      fmt::print("{}\n", FtxUiTemplate::cmake::project_version);
       return EXIT_SUCCESS;
     }
 
